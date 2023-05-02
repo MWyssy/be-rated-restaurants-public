1addb58d56011247bec39bfb8f3c169373a89000
MWyssy <mike@wyss.co.uk>
2023-05-02 15:33:27 +0300

    add more setup

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
-
+const { Pool } = require('pg');
+const db = new Pool();
 const app = express();
 
+if (!process.env.PGDATABASE) {
+    throw new Error('No PGDATASE configured!');
+}
+
+module.exports = app;
--- listener.js
+++ listener.js
@@ -1,3 +1,6 @@
-const { Pool } = require('pg');
-const db = new Pool();
+const app = require('./app.js');
 
+app.listen(9090, (err) => {
+    if (err) throw new Error(err)
+    else console.log('Server is listening on port 9090...');
+});
